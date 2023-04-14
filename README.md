e5e0dff6b09bb30b8ce0403d339bfe68cee3e612
pi0neerpat <blockchainbuddha@gmail.com>
2023-04-14 13:23:35 -0700

    init2

---
+++ README.md
@@ -0,0 +1,51 @@
+# Chat GPT Playground
+
+> Guide for anyone to connect to a paperspace server and access GPT Agent tools
+
+
+## Setup
+
+Create SSH keys and copy the contents of the file ending in `.pub` 
+
+https://www.howtogeek.com/762863/how-to-generate-ssh-keys-in-windows-10-and-windows-11/
+
+Install Git
+
+https://git-scm.com/download/win
+
+Install Node.js and NPM
+
+https://phoenixnap.com/kb/install-node-js-npm-on-windows
+
+Clone this repo
+
+```bash
+git clone https://github.com/pi0neerpat/gpt-playground.git
+```
+
+Install 
+
+```
+cd gpt-playground
+npm i
+```
+
+Create a file called `.env` and paste this content with the api key
+
+```
+PAPERSPACE_API_KEY=
+```
+
+## Usage 
+
+Start the program with 
+
+```
+npm start
+```
+
+## Resources
+
+- Prompt writing resources https://github.com/f/awesome-chatgpt-prompts
+- Desktop App https://github.com/lencx/ChatGPT
+- Steamship tool for hosting a chatbot https://www.steamship.com/build?utm_source=github&utm_medium=explainer&utm_campaign=awesome_gpt_prompts&utm_id=awesome_gpt_prompts
---
+++ paperspace-start.js
@@ -0,0 +1,93 @@
+const paperspace_node = require('paperspace-node');
+const readline = require('readline');
+const dotenv = require('dotenv');
+dotenv.config();
+const { exec } = require('child_process');
+
+var paperspace = paperspace_node({
+    apiKey: process.env.PAPERSPACE_API_KEY,
+});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function execWaitForOutput(command, execOptions = {}) {
+    return new Promise((resolve, reject) => {
+        const childProcess = exec(command, execOptions);
+
+        // stream process output to console
+        childProcess.stderr.on('data', data => console.error(data));
+        childProcess.stdout.on('data', data => console.log(data));
+        // handle exit
+        childProcess.on('exit', () => resolve());
+        childProcess.on('close', () => resolve());
+        // handle errors
+        childProcess.on('error', error => reject(error));
+    })
+}
+
+const onMachineStart = async (machine) => {
+    rl.question(`Do you want to start Auto-GPT? (y/n): `, (answer) => {
+        if (answer === 'y') {
+            console.log(`*****************\nRun these commands:\n\nssh paperspace@${machine.publicIpAddress}\ncd Auto-GPT\npython3 scripts/main.py\n\n********************`)
+            execWaitForOutput(`code --remote ssh-remote+paperspace@${machine.publicIpAddress} /home/paperspace/`)
+        } else if (answer === 'n') {
+            console.log(`Connect to Stable Diffusion with: ssh -L 127.0.0.1:3001:127.0.0.1:3001 paperspace@${machine.publicIpAddress}`);
+            execWaitForOutput(`code --remote ssh-remote+paperspace@${machine.publicIpAddress} /home/paperspace/`)
+        } else {
+            console.log('Invalid input. Please enter "y" or "n".');
+        }
+        rl.close();
+    });
+}
+
+
+const useMachine = (machine) => {
+    if (machine.state === "ready") {
+        rl.question(`Machine ${machine.id} is ready! Do you want to stop it? (y/n): `, (answer) => {
+            if (answer === 'y') {
+                console.log('Stopping machine...');
+                paperspace.machines.stop({ machineId: machine.id },
+                    function (err, res) {
+                        if (err) {
+                            console.log(err.response.error.text)
+                        } else if (res) {
+                            console.log('Machine stopped!');
+                        }
+                    }
+                );
+            } else if (answer === 'n') {
+                return onMachineStart(machine)
+            } else {
+                console.log('Invalid input. Please enter "y" or "n".');
+            }
+            rl.close();
+        });
+    } else {
+        console.log(`Machine ${machine.id} is not ready!`);
+        paperspace.machines.start({ machineId: machine.id }, (err, machine) => {
+            if (err) {
+                console.log(err.response.error.text)
+            }
+            console.log(`Starting machine. Retry in 10 seconds.`);
+            rl.close();
+        });
+    }
+}
+
+const selectMachine = () => paperspace.machines.list(function (err, res) {
+    console.log(res.map(m => `${m.id} "${m.name}" (${m.state}) ${m.gpu} GPU, ${m.cpus} CPUs, ${Math.floor(Number(m.ram) / 1000000000)} GB ram`))
+    rl.question(`Which machine would you like to use? Machine ID: `, (answer) => {
+        machine = res.find(m => m.id === answer);
+        if (machine) {
+            return useMachine(machine)
+        } else {
+            console.log('Machine not found');
+        }
+        rl.close();
+    });
+});
+
+selectMachine()
